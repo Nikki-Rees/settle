2c5a2a21022f6590b7e4f35f592dc5cc1ec17a6d
Nikki-Rees <nikki-rees@outlook.com>
2020-10-28 00:39:43 +1100

    save change to server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ passport.deserializeUser(User.deserializeUser());
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  process.env.MONGODB_URI || 'mongodb://localhost/settle',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
